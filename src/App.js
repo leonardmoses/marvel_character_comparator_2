698e37e851ccfdd89149273252f863b020181956
Qalexeon <leonardsamuelmoses@gmail.com>
2023-08-22 15:28:46 -0400

    Fixed problem with 'uncaught typeerror:' Problem occurred when inital state set to default value not having a value. Fixed when using a ternary operator to first check if data exists. If so then return data, otherwise return 'loading...'

--- src/App.js
+++ src/App.js
@@ -4,13 +4,36 @@ import Splash from "./pages/Splash";
 import Comparator from "./pages/Comparator";
 import Directions from "./pages/Directions";
 import Layout from "./components/layouts/Layout";
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 
 function App() {
+
+  const URL = "https://www.superheroapi.com/api.php/10158052899366078/search/hulk"
+
+
+  const [charData , setCharData ] = useState('')
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(URL);
+      // console.log(response.data.results)
+      setCharData(response.data.results)
+      // console.log(response.data.results)
+    } catch(error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Routes>
       <Route element={<Splash />} path="/" />
-      <Route element={<Layout><Comparator /></Layout>} path="/comparator"/>
+      <Route element={<Layout><Comparator charData={charData}/></Layout>} path="/comparator"/>
       <Route element={<Layout><Directions/></Layout>} path="/directions"/>
     </Routes>
   );
--- src/pages/Comparator.js
+++ src/pages/Comparator.js
@@ -1,8 +1,36 @@
-const Comparator = () => {
+
+
+const Comparator = (props) => {
+
+  console.log(props.charData)
+
+  // const name = props.charData[0].name
+  // const combat = props.charData[0].combat
+  // const durability = props.charData[0].durability
+  // const intelligence = props.charData[0].intelligence
+  // const power = props.charData[0].power
+  // const speed = props.charData[0].speed
+  // const strength = props.charData[0].strength
+  
+  function renderInfo() {
+    return (
+      <div>
+        <h2>Name: {props.charData[0].name}</h2>
+        <h3>Combat: {props.charData[0].powerstats.combat}</h3>
+        <h3>power: {props.charData[0].powerstats.power}</h3>
+        <h3>strength: {props.charData[0].powerstats.strength}</h3>
+        <h3>speed: {props.charData[0].powerstats.speed}</h3>
+        <h3>durability: {props.charData[0].powerstats.durability}</h3>
+        <h3>intelligence: {props.charData[0].powerstats.intelligence}</h3>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <h1>Comparator</h1>
+      {props.charData ? renderInfo() : 'loading...'}
     </div>
+  
   );
 };
 
