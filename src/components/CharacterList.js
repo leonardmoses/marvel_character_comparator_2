34823c7ca88acb50f51d052b8ad13f56201e3d69
Qalexeon <leonardsamuelmoses@gmail.com>
2023-09-04 14:42:49 -0400

    Populated CharacterList Component. Hard copy and pastes names and id in csv and manually converted to array of objects. Imported totalCharList into component and mapped to render in list items.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,30 +1,21 @@
-import './CharacterList.scss';
+import {totalCharList} from "../data/TotalCharList.js"
+import "./CharacterList.scss";
 
 const CharcterList = (props) => {
 
-    const marvelTotalCharList = [];
-    const hardCodeMaxChar = 100;
+  return (
+    <div className="CharacterListBody">
+      <h2>List of Searchable Characters</h2>
+      <button onClick={props.onClose}>Close</button>
+      <div className="ListContainer">
+        <ul>
+          {totalCharList.map((character, idx) => (
+            <li key={idx}>{character.id} {character.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-    for (let i=1; i<hardCodeMaxChar+1; i++) {
-        marvelTotalCharList.push(`Character ${i}`)
-    }
-
-    console.log(marvelTotalCharList)
-
-    return ( 
-        <div className='CharacterListBody'>
-            <h2>List of Marvel Characters</h2>
-            <button onClick={props.onClose}>Close</button>
-            <div className='ListContainer'>
-                <ul>
-                {marvelTotalCharList.map((x , idx)  => (
-                    <li>{x}</li>
-                ))}
-                </ul>
-            </div>
-
-        </div>
-     );
-}
- 
 export default CharcterList;
