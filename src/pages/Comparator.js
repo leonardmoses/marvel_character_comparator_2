5d06123aaea0d6edfd7fe0bbb4f6cdd0d55cdcff
Qalexeon <leonardsamuelmoses@gmail.com>
2023-09-07 22:40:28 -0400

    Refactored SingleCharacter like Comparator.

--- src/pages/Comparator.js
+++ src/pages/Comparator.js
@@ -38,134 +38,136 @@ const Comparator = (props) => {
   return (
     <div className="ComparatorBody">
       <h3>Vs</h3>
+
       <div className="ComparatorContent">
-      <div className="Hero1">
-        <Char1SearchForm
-          submitName1Input={props.submitName1Input}
-          setSelectedCharacter1={setSelectedCharacter1}
-          baseURL={props.baseURL}
-          setUrl1={props.setUrl1}
-          charData1={props.charData1}
-        />
-        <div className="ListOfNames1">
-          {listOfNames1.map((name, idx) => (
-            <button key={idx} value={idx} onClick={selectCharacter1FromList}>
-              {name}
-            </button>
-          ))}
-        </div>
 
-        <div className="CharacterData">
-        
-          <div className="ImageBackground">
-            <div className="ImageContainer">
-              <img src={selectedCharacter1?.image?.url ? selectedCharacter1?.image?.url: "https://www.superherodb.com/gallery2/075/157/15709.webp"} alt={selectedCharacter1?.name}/>
-            </div>
+        <div className="Hero1">
+          <Char1SearchForm
+            submitName1Input={props.submitName1Input}
+            setSelectedCharacter1={setSelectedCharacter1}
+            baseURL={props.baseURL}
+            setUrl1={props.setUrl1}
+            charData1={props.charData1}
+          />
+          <div className="ListOfNames1">
+            {listOfNames1.map((name, idx) => (
+              <button key={idx} value={idx} onClick={selectCharacter1FromList}>
+                {name}
+              </button>
+            ))}
           </div>
 
-          <div className="CharacterInfo">
-            <div className="Name1"><h4>{selectedCharacter1?.name ? selectedCharacter1?.name : "Alias"}</h4></div>
-            <div className="FullName1"><h4>{selectedCharacter1?.biography?.["full-name"] ? selectedCharacter1?.biography?.["full-name"] : "Full Name"}</h4></div>
-
-            <div className="Stats">
-              <h4>Power Stats</h4>
-              <h5 className="InfoType">Combat:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.combat}</h5>
-              <h5 className="InfoType">Power:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.power}</h5>
-              <h5 className="InfoType">Strength:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.strength}</h5>
-              <h5 className="InfoType">Speed:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.speed}</h5>
-              <h5 className="InfoType">Durability:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.durability}</h5>
-              <h5 className="InfoType">Intelligence:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.intelligence}</h5>
+          <div className="CharacterData">
+          
+            <div className="ImageBackground">
+              <div className="ImageContainer">
+                <img src={selectedCharacter1?.image?.url ? selectedCharacter1?.image?.url: "https://www.superherodb.com/gallery2/075/157/15709.webp"} alt={selectedCharacter1?.name}/>
+              </div>
             </div>
 
-            <div className="Bio">
-              <h4>Appearance</h4>
-              <h5 className="InfoType">Gender:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.gender}</h5>
-              <h5 className="InfoType">Height:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.height}</h5>
-              <h5 className="InfoType">Weight:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.weight}</h5>
-              <h5 className="InfoType">Eye Color:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.["eye-color"]}</h5>
-              <h5 className="InfoType">Hair Color:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.["hair-color"]}</h5>
-              <h5 className="InfoType">Race:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.race}</h5>
+            <div className="CharacterInfo">
+              <div className="Name1"><h4>{selectedCharacter1?.name ? selectedCharacter1?.name : "Alias"}</h4></div>
+              <div className="FullName1"><h4>{selectedCharacter1?.biography?.["full-name"] ? selectedCharacter1?.biography?.["full-name"] : "Full Name"}</h4></div>
+
+              <div className="Stats">
+                <h4>Power Stats</h4>
+                <h5 className="InfoType">Combat:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.combat}</h5>
+                <h5 className="InfoType">Power:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.power}</h5>
+                <h5 className="InfoType">Strength:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.strength}</h5>
+                <h5 className="InfoType">Speed:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.speed}</h5>
+                <h5 className="InfoType">Durability:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.durability}</h5>
+                <h5 className="InfoType">Intelligence:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.intelligence}</h5>
+              </div>
+
+              <div className="Bio">
+                <h4>Appearance</h4>
+                <h5 className="InfoType">Gender:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.gender}</h5>
+                <h5 className="InfoType">Height:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.height}</h5>
+                <h5 className="InfoType">Weight:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.weight}</h5>
+                <h5 className="InfoType">Eye Color:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.["eye-color"]}</h5>
+                <h5 className="InfoType">Hair Color:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.["hair-color"]}</h5>
+                <h5 className="InfoType">Race:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.race}</h5>
+              </div>
+
+              <div className="Intel">
+                <h4>Intel</h4>
+                <h5 className="InfoType">Place of birth:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.["place-of-birth"]}</h5>
+                <h5 className="InfoType">Percieved Moral Alignment:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.alignment}</h5>
+                <h5 className="InfoType">Aliases:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.aliases}</h5>
+                <h5 className="InfoType">Alter Egos:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.["alter-egos"]}</h5>
+                <h5 className="InfoType">Relatives:</h5><h5 className="InfoValue">{selectedCharacter1?.connections?.relatives}</h5>
+                <h5 className="InfoType">Group Affliations:</h5><h5 className="InfoValue">{selectedCharacter1?.connections?.["group-affiliation"]}</h5>
+                <h5 className="InfoType">Occupations:</h5><h5 className="InfoValue">{selectedCharacter1?.work?.occupation}</h5>
+                <h5 className="InfoType">Base of Operations:</h5><h5 className="InfoValue">{selectedCharacter1?.work?.base}</h5>
+              </div>
             </div>
 
-            <div className="Intel">
-              <h4>Intel</h4>
-              <h5 className="InfoType">Place of birth:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.["place-of-birth"]}</h5>
-              <h5 className="InfoType">Percieved Moral Alignment:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.alignment}</h5>
-              <h5 className="InfoType">Aliases:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.aliases}</h5>
-              <h5 className="InfoType">Alter Egos:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.["alter-egos"]}</h5>
-              <h5 className="InfoType">Relatives:</h5><h5 className="InfoValue">{selectedCharacter1?.connections?.relatives}</h5>
-              <h5 className="InfoType">Group Affliations:</h5><h5 className="InfoValue">{selectedCharacter1?.connections?.["group-affiliation"]}</h5>
-              <h5 className="InfoType">Occupations:</h5><h5 className="InfoValue">{selectedCharacter1?.work?.occupation}</h5>
-              <h5 className="InfoType">Base of Operations:</h5><h5 className="InfoValue">{selectedCharacter1?.work?.base}</h5>
-            </div>
+
+          </div>
+        </div>
+
+        <div className="Hero2">
+          <Char2SearchForm
+            submitName2Input={props.submitName2Input}
+            setSelectedCharacter2={setSelectedCharacter2}
+            baseURL={props.baseURL}
+            setUrl2={props.setUrl2}
+            charData2={props.charData2}
+          />
+
+          <div className="ListOfNames2">
+            {listOfNames2.map((name, idx) => (
+              <button key={idx} value={idx} onClick={selectCharacter2FromList}>
+                {name}
+              </button>
+            ))}
           </div>
 
 
-        </div>
-      </div>
-
-      <div className="Hero2">
-        <Char2SearchForm
-          submitName2Input={props.submitName2Input}
-          setSelectedCharacter2={setSelectedCharacter2}
-          baseURL={props.baseURL}
-          setUrl2={props.setUrl2}
-          charData2={props.charData2}
-        />
-
-        <div className="ListOfNames2">
-          {listOfNames2.map((name, idx) => (
-            <button key={idx} value={idx} onClick={selectCharacter2FromList}>
-              {name}
-            </button>
-          ))}
-        </div>
-
-
-        <div className="CharacterData">
-          <div className="ImageBackground">
-            <div className="ImageContainer">
-              <img src={selectedCharacter2?.image?.url ? selectedCharacter2?.image?.url: "https://www.superherodb.com/gallery2/075/452/45242.webp"} alt={selectedCharacter2?.name}/>
+          <div className="CharacterData">
+            <div className="ImageBackground">
+              <div className="ImageContainer">
+                <img src={selectedCharacter2?.image?.url ? selectedCharacter2?.image?.url: "https://www.superherodb.com/gallery2/075/452/45242.webp"} alt={selectedCharacter2?.name}/>
+              </div>
             </div>
+            <div className="CharacterInfo">
+              <div className="Name2"><h4>{selectedCharacter2?.name ? selectedCharacter2?.name : "Alias"}</h4></div>
+              <div className="FullName2"><h4>{selectedCharacter2?.biography?.["full-name"] ? selectedCharacter2?.biography?.["full-name"] : "Full Name"}</h4></div>
+              <div className="Stats">
+                <h4>Power Stats</h4>
+                <h5 className="InfoType">Combat:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.combat}</h5>
+                <h5 className="InfoType">Power:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.power}</h5>
+                <h5 className="InfoType">Strength:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.strength}</h5>
+                <h5 className="InfoType">Speed:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.speed}</h5>
+                <h5 className="InfoType">Durability:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.durability}</h5>
+                <h5 className="InfoType">Intelligence:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.intelligence}</h5>
+              </div>
+
+              <div className="Bio">
+                <h4>Appearance</h4>
+                <h5 className="InfoType">Gender:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.gender}</h5>
+                <h5 className="InfoType">Height:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.height}</h5>
+                <h5 className="InfoType">Weight:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.weight}</h5>
+                <h5 className="InfoType">Eye Color:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.["eye-color"]}</h5>
+                <h5 className="InfoType">Hair Color:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.["hair-color"]}</h5>
+                <h5 className="InfoType">Race:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.race}</h5>
+              </div>
+
+              <div className="Intel">
+                <h4>Intel</h4>
+                <h5 className="InfoType">Place of birth:</h5><h5 className="InfoValue">{selectedCharacter2?.biography?.["place-of-birth"]}</h5>
+                <h5 className="InfoType">Percieved Moral Alignment:</h5><h5 className="InfoValue">{selectedCharacter2?.biography?.alignment}</h5>
+                <h5 className="InfoType">Aliases:</h5><h5 className="InfoValue">{selectedCharacter2?.biography?.aliases}</h5>
+                <h5 className="InfoType">Alter Egos:</h5><h5 className="InfoValue">{selectedCharacter2?.biography?.["alter-egos"]}</h5>
+                <h5 className="InfoType">Relatives:</h5><h5 className="InfoValue">{selectedCharacter2?.connections?.relatives}</h5>
+                <h5 className="InfoType">Group Affliations:</h5><h5 className="InfoValue">{selectedCharacter2?.connections?.["group-affiliation"]}</h5>
+                <h5 className="InfoType">Occupations:</h5><h5 className="InfoValue">{selectedCharacter2?.work?.occupation}</h5>
+                <h5 className="InfoType">Base of Operations:</h5><h5 className="InfoValue">{selectedCharacter2?.work?.base}</h5>
+              </div>
+            </div>
+
           </div>
-          <div className="CharacterInfo">
-            <div className="Name2"><h4>{selectedCharacter2?.name ? selectedCharacter2?.name : "Alias"}</h4></div>
-            <div className="FullName2"><h4>{selectedCharacter2?.biography?.["full-name"] ? selectedCharacter2?.biography?.["full-name"] : "Full Name"}</h4></div>
-            <div className="Stats">
-              <h4>Power Stats</h4>
-              <h5 className="InfoType">Combat:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.combat}</h5>
-              <h5 className="InfoType">Power:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.power}</h5>
-              <h5 className="InfoType">Strength:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.strength}</h5>
-              <h5 className="InfoType">Speed:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.speed}</h5>
-              <h5 className="InfoType">Durability:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.durability}</h5>
-              <h5 className="InfoType">Intelligence:</h5><h5 className="InfoValue">{selectedCharacter2?.powerstats?.intelligence}</h5>
-            </div>
-
-            <div className="Bio">
-              <h4>Appearance</h4>
-              <h5 className="InfoType">Gender:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.gender}</h5>
-              <h5 className="InfoType">Height:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.height}</h5>
-              <h5 className="InfoType">Weight:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.weight}</h5>
-              <h5 className="InfoType">Eye Color:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.["eye-color"]}</h5>
-              <h5 className="InfoType">Hair Color:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.["hair-color"]}</h5>
-              <h5 className="InfoType">Race:</h5><h5 className="InfoValue">{selectedCharacter2?.appearance?.race}</h5>
-            </div>
-
-            <div className="Intel">
-              <h4>Intel</h4>
-              <h5 className="InfoType">Place of birth:</h5><h5 className="InfoValue">{selectedCharacter2?.biography?.["place-of-birth"]}</h5>
-              <h5 className="InfoType">Percieved Moral Alignment:</h5><h5 className="InfoValue">{selectedCharacter2?.biography?.alignment}</h5>
-              <h5 className="InfoType">Aliases:</h5><h5 className="InfoValue">{selectedCharacter2?.biography?.aliases}</h5>
-              <h5 className="InfoType">Alter Egos:</h5><h5 className="InfoValue">{selectedCharacter2?.biography?.["alter-egos"]}</h5>
-              <h5 className="InfoType">Relatives:</h5><h5 className="InfoValue">{selectedCharacter2?.connections?.relatives}</h5>
-              <h5 className="InfoType">Group Affliations:</h5><h5 className="InfoValue">{selectedCharacter2?.connections?.["group-affiliation"]}</h5>
-              <h5 className="InfoType">Occupations:</h5><h5 className="InfoValue">{selectedCharacter2?.work?.occupation}</h5>
-              <h5 className="InfoType">Base of Operations:</h5><h5 className="InfoValue">{selectedCharacter2?.work?.base}</h5>
-            </div>
-          </div>
-
         </div>
-      </div>
 
       </div>
     </div>
--- src/pages/SingleCharacter.js
+++ src/pages/SingleCharacter.js
@@ -24,64 +24,66 @@ const SingleCharacter = (props) => {
     <div className="SingleCharacterBody">
       <h3>Single</h3>
 
-      <Char1SearchForm
-        submitName1Input={props.submitName1Input}
-        setSelectedCharacter1={setSelectedCharacter1}
-        baseURL={props.baseURL}
-        setUrl1={props.setUrl1}
-        charData1={props.charData1}
-      />
-      
-      <div className="ListOfNames">
-        {listOfNames.map((name, idx) => (
-          <button key={idx} value={idx} onClick={selectCharacterFromList}>
-            {name}
-          </button>
-        ))}
-      </div>
+      <div className="Hero1">
+        <Char1SearchForm
+          submitName1Input={props.submitName1Input}
+          setSelectedCharacter1={setSelectedCharacter1}
+          baseURL={props.baseURL}
+          setUrl1={props.setUrl1}
+          charData1={props.charData1}
+        />
+        
+        <div className="ListOfNames">
+          {listOfNames.map((name, idx) => (
+            <button key={idx} value={idx} onClick={selectCharacterFromList}>
+              {name}
+            </button>
+          ))}
+        </div>
 
-        <div className="CharacterData">
+          <div className="CharacterData">
 
-          <div className="ImageBackground">
-            <div className="ImageContainer">
-              <img src={selectedCharacter1?.image?.url ? selectedCharacter1?.image?.url: "https://www.superherodb.com/gallery2/075/157/15709.webp"} alt={selectedCharacter1?.name}/>
+            <div className="ImageBackground">
+              <div className="ImageContainer">
+                <img src={selectedCharacter1?.image?.url ? selectedCharacter1?.image?.url: "https://www.superherodb.com/gallery2/075/157/15709.webp"} alt={selectedCharacter1?.name}/>
+              </div>
             </div>
-          </div>
 
-          <div className="CharacterInfo">
-            <h3 className="Name">{selectedCharacter1?.name ? selectedCharacter1?.name : "—"}</h3>
-          <div className="FullName">{selectedCharacter1?.biography?.["full-name"]}</div>
+            <div className="CharacterInfo">
+              <div className="Name"><h4>{selectedCharacter1?.name ? selectedCharacter1?.name : "—"}</h4></div>
+            <div className="FullName"><h4>{selectedCharacter1?.biography?.["full-name"] ? selectedCharacter1?.biography?.["full-name"] : "—"}</h4></div>
 
-          <div className="Stats">
-            <h3>Power Stats</h3>
-            <h5 className="InfoType">Combat:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.combat}</h5>
-            <h5 className="InfoType">power:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.power}</h5>
-            <h5 className="InfoType">strength:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.strength}</h5>
-            <h5 className="InfoType">speed:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.speed}</h5>
-            <h5 className="InfoType">durability:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.durability}</h5>
-            <h5 className="InfoType">intelligence:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.intelligence}</h5>
-          </div>
+            <div className="Stats">
+              <h4>Power Stats</h4>
+              <h5 className="InfoType">Combat:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.combat}</h5>
+              <h5 className="InfoType">power:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.power}</h5>
+              <h5 className="InfoType">strength:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.strength}</h5>
+              <h5 className="InfoType">speed:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.speed}</h5>
+              <h5 className="InfoType">durability:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.durability}</h5>
+              <h5 className="InfoType">intelligence:</h5><h5 className="InfoValue">{selectedCharacter1?.powerstats?.intelligence}</h5>
+            </div>
 
-          <div className="Bio">
-            <h3>Appearance</h3>
-            <h5 className="InfoType">Gender:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.gender}</h5>
-            <h5 className="InfoType">Height:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.height}</h5>
-            <h5 className="InfoType">Weight:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.weight}</h5>
-            <h5 className="InfoType">Eye Color:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.["eye-color"]}</h5>
-            <h5 className="InfoType">Hair Color:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.["hair-color"]}</h5>
-            <h5 className="InfoType">Race:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.race}</h5>
-          </div>
+            <div className="Bio">
+              <h4>Appearance</h4>
+              <h5 className="InfoType">Gender:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.gender}</h5>
+              <h5 className="InfoType">Height:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.height}</h5>
+              <h5 className="InfoType">Weight:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.weight}</h5>
+              <h5 className="InfoType">Eye Color:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.["eye-color"]}</h5>
+              <h5 className="InfoType">Hair Color:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.["hair-color"]}</h5>
+              <h5 className="InfoType">Race:</h5><h5 className="InfoValue">{selectedCharacter1?.appearance?.race}</h5>
+            </div>
 
-          <div className="Intel">
-            <h3>Intel</h3>
-            <h5 className="InfoType">Place of birth:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.["place-of-birth"]}</h5>
-            <h5 className="InfoType">Percieved Moral Alignment:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.alignment}</h5>
-            <h5 className="InfoType">Aliases:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.aliases}</h5>
-            <h5 className="InfoType">Alter Egos:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.["alter-egos"]}</h5>
-            <h5 className="InfoType">Relatives:</h5><h5 className="InfoValue">{selectedCharacter1?.connections?.relatives}</h5>
-            <h5 className="InfoType">Group Affliations:</h5><h5 className="InfoValue">{selectedCharacter1?.connections?.["group-affiliation"]}</h5>
-            <h5 className="InfoType">Occupations:</h5><h5 className="InfoValue">{selectedCharacter1?.work?.occupation}</h5>
-            <h5 className="InfoType">Base of Operations:</h5><h5 className="InfoValue">{selectedCharacter1?.work?.base}</h5>
+            <div className="Intel">
+              <h4>Intel</h4>
+              <h5 className="InfoType">Place of birth:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.["place-of-birth"]}</h5>
+              <h5 className="InfoType">Percieved Moral Alignment:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.alignment}</h5>
+              <h5 className="InfoType">Aliases:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.aliases}</h5>
+              <h5 className="InfoType">Alter Egos:</h5><h5 className="InfoValue">{selectedCharacter1?.biography?.["alter-egos"]}</h5>
+              <h5 className="InfoType">Relatives:</h5><h5 className="InfoValue">{selectedCharacter1?.connections?.relatives}</h5>
+              <h5 className="InfoType">Group Affliations:</h5><h5 className="InfoValue">{selectedCharacter1?.connections?.["group-affiliation"]}</h5>
+              <h5 className="InfoType">Occupations:</h5><h5 className="InfoValue">{selectedCharacter1?.work?.occupation}</h5>
+              <h5 className="InfoType">Base of Operations:</h5><h5 className="InfoValue">{selectedCharacter1?.work?.base}</h5>
+            </div>
           </div>
         </div>
       </div>
